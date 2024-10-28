58d26b746f592e1a4730d3125b931568a136b1d0
Souravangralp <122254035+Souravangralp@users.noreply.github.com>
2024-10-28 17:46:14 +0530

    done bug fixes

--- obj/Debug/net8.0/QuickBookApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuickBookApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c52b64f5d72f97a1bf2d72cc08a6b35a4921ee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bbb0d1a44f34c1b19cfc487655cb50dc4c8a7c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
